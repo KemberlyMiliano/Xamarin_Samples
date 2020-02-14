c6564c87773b100fe9aa1ca2cd4b65d01860887c
KemberlyMiliano <kemberlymiliano@gmail.com>
2020-02-13 20:47:11 -0400

    Add Project File

---
+++ AppXample/AppXample/AppXample/DiscoveryPage.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppXample
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DiscoveryPage : ContentPage
+    {
+        public DiscoveryPage()
+        {
+            InitializeComponent();
+
+        }
+        private void Stack_Clicked(object sender, EventArgs e)
+        {
+            beerStack.BackgroundColor = Color.FromHex("#F9A602");
+            diningStack.BackgroundColor = Color.White;
+            cafeStack.BackgroundColor = Color.White;
+            nearbyStack.BackgroundColor = Color.White;
+            foodStack.BackgroundColor = Color.White;
+            pizzaStack.BackgroundColor = Color.White;
+        }
+        private void Stack_Clicked1(object sender, EventArgs e)
+        {
+            diningStack.BackgroundColor = Color.FromHex("#F9A602");
+            beerStack.BackgroundColor = Color.White;
+            cafeStack.BackgroundColor = Color.White;
+            nearbyStack.BackgroundColor = Color.White;
+            foodStack.BackgroundColor = Color.White;
+            pizzaStack.BackgroundColor = Color.White;
+        }
+        private void Stack_Clicked2(object sender, EventArgs e)
+        {
+            cafeStack.BackgroundColor = Color.FromHex("#F9A602");
+            diningStack.BackgroundColor = Color.White; ;
+            beerStack.BackgroundColor = Color.White;
+            nearbyStack.BackgroundColor = Color.White;
+            foodStack.BackgroundColor = Color.White;
+            pizzaStack.BackgroundColor = Color.White;
+        }
+        private void Stack_Clicked3(object sender, EventArgs e)
+        {
+            nearbyStack.BackgroundColor = Color.FromHex("#F9A602");
+            cafeStack.BackgroundColor = Color.White;
+            diningStack.BackgroundColor = Color.White; ;
+            beerStack.BackgroundColor = Color.White;
+            foodStack.BackgroundColor = Color.White;
+            pizzaStack.BackgroundColor = Color.White;
+        }
+        private void Stack_Clicked4(object sender, EventArgs e)
+        {
+            foodStack.BackgroundColor = Color.FromHex("#F9A602");
+            nearbyStack.BackgroundColor = Color.White;
+            cafeStack.BackgroundColor = Color.White;
+            diningStack.BackgroundColor = Color.White; ;
+            beerStack.BackgroundColor = Color.White;
+            pizzaStack.BackgroundColor = Color.White;
+        }
+        private void Stack_Clicked5(object sender, EventArgs e)
+        {
+            pizzaStack.BackgroundColor = Color.FromHex("#F9A602");
+            foodStack.BackgroundColor = Color.White;
+            nearbyStack.BackgroundColor = Color.White;
+            cafeStack.BackgroundColor = Color.White;
+            diningStack.BackgroundColor = Color.White; ;
+            beerStack.BackgroundColor = Color.White;
+        }
+
+    }
+}
---
+++ AppXample/AppXample/AppXample/LoginPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppXample
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+        private async void SignUp_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SignUpPage());
+
+        }
+        private void Login_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(username.Text))
+            {
+                DisplayAlert("Alert", "Please enter your username or email address", "OK");
+            }
+            else if (string.IsNullOrEmpty(password.Text))
+            {
+                DisplayAlert("Alert", "Please enter your password", "OK");
+            }
+            else if (string.IsNullOrEmpty(username.Text) && string.IsNullOrEmpty(password.Text))
+            {
+                DisplayAlert("Alert", "Please enter your username and password", "OK");
+            }
+            else
+            {
+                Navigation.PushAsync(new MainPage());
+
+            }
+        }
+        void OnChangePasswordVisibility(System.Object sender, System.EventArgs e)
+        {
+            imageShowPassword.Source = !password.IsPassword ? "eyeIcon" : "closeEyeIcon";
+            password.IsPassword = !password.IsPassword;
+
+        }
+
+    }
+}
